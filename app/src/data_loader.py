291e01c0941d8bd509a804015f13ce7abf351eb4
Y-DONALD <donaldyoungoua@gmail.com>
2025-01-31 04:53:03 +0100

    Correct surevaluation problem

--- app/src/data_loader.py
+++ app/src/data_loader.py
@@ -4,12 +4,15 @@ from tensorflow.keras.utils import image_dataset_from_directory
 
 
 def load_dataset(dataset_path, img_size=(600, 600), batch_size=32):
+    # Define seed
+    # SEED = np.random.randint(0, 10000)
+    SEED = 142
     # charge the dataset en divided it in en train/test set
     train_dataset = image_dataset_from_directory(
         dataset_path,
         validation_split=0.2,
         subset="training",
-        seed=123,
+        seed=SEED,
         image_size=img_size,
         batch_size=batch_size
     )
@@ -17,7 +20,7 @@ def load_dataset(dataset_path, img_size=(600, 600), batch_size=32):
         dataset_path,
         validation_split=0.2,
         subset="validation",
-        seed= np.random.randint(0, 10000),
+        seed= SEED,
         image_size=img_size,
         batch_size=batch_size
     )
